2914334db26ba08f5f0c47867731a077a261ca7e
Odeyalo <theprogrammerinfuture@gmail.com>
2022-08-12 20:20:02 +0300

    Added task description

--- src/edu/odeyalotasks/second/Main.java
+++ src/edu/odeyalotasks/second/Main.java
@@ -2,6 +2,17 @@ package edu.odeyalotasks.second;
 
 import edu.odeyalotasks.second.config.SecondTaskApplicationConfiguration;
 
+/**
+ * Завдання 2. Наступне та попереднє
+ * Напишіть програму, яка зчитує ціле число та виводить текст, аналогічний наведеному у
+ * прикладі. Прогалини, розділові знаки, великі і малі літери важливі!
+ * Вхідні дані
+ * Вводиться ціле число, за модулем, що не перевищує 10000.
+ * Вихідні дані
+ * Виведіть спочатку фразу "The next number for the number", потім введене число, потім
+ * слово "is", оточене пробілами, потім формулу для наступного за введеним числом,
+ * нарешті, знак точки без пробілу. Аналогічно у наступному рядку для попереднього числа.
+ */
 public class Main {
     public static void main(String[] args) {
         SecondTask.run(new SecondTaskApplicationConfiguration());
