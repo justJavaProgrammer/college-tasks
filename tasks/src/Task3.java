429d3268ae2887867da56f24a80ce4052163ce91
Odeyalo <theprogrammerinfuture@gmail.com>
2022-08-19 15:11:27 +0300

    Added task #3

---
+++ tasks/src/Task3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+/**
+ * Завдання 3. Поділ яблук - 1
+ * N школярів ділять K яблук порівну, залишок, що не ділиться, залишається в кошику.
+ * Скільки яблук дістанеться кожному школяреві?
+ * Вхідні дані
+ * Програма отримує вхід числа N і K.
+ * Вихідні дані
+ * Програма повинна вивести кількість яблук.
+ * Приклади
+ * вхідні дані
+ * 3
+ * 14
+ * вихідні дані
+ * 4
+ */
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int students = scn.nextInt();
+        System.out.print("Enter number of apples: ");
+        int apples = scn.nextInt();
+        int formula = (int) apples / students;
+        System.out.println("Every student will get: " + formula);
+    }
+}
